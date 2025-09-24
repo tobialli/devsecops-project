42eb0540d995d8da5aac8c513bfb3da96eb22aab
tobialli <tobifulania@outlook.com>
2025-09-24 18:39:06 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
+}
+terraform {
+  backend "s3" {
+    bucket         = "terraformbuckettoday121"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-west-2"
+  }
 }
